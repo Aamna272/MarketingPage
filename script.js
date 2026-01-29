d8be859507db0141f0e2d8f20e641da6d6f91f29
Aamna272 <aamnakainat19@gmail.com>
2026-01-29 19:28:30 +0500

    responsiveness issues fixed

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
-function toggleFaq(button) {
-  const faqItem = button.closest(".faq-item");
-  const text = faqItem.querySelector(".faq-text");
-  const icon = faqItem.querySelector(".faq-icon");
+// function toggleFaq(button) {
+//   const faqItem = button.closest(".faq-item");
+//   const text = faqItem.querySelector(".faq-text");
+//   const icon = faqItem.querySelector(".faq-icon");
 
-  const isHidden = text.classList.contains("hidden");
+//   const isHidden = text.classList.contains("hidden");
 
-  text.classList.toggle("hidden");
+//   text.classList.toggle("hidden");
 
-  icon.src = isHidden ? "./images/minus.svg" : "./images/plus.svg";
-}
+//   icon.src = isHidden ? "./images/minus.svg" : "./images/plus.svg";
+// }
+const ele = document.querySelectorAll("insetShadow");
+
+ele.addEventListener("focus", () => {
+  ele.classList.add("custom-inset-shadow");
+});
+
+ele.addEventListener("blur", () => {
+  inputElement.classList.remove("custom-inset-shadow");
+});
